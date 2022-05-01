72e87c5bf020d8ece07f262e2d92a469695cc13a
AFSantos00 <95322131+AFSantos00@users.noreply.github.com>
2022-05-01 18:34:33 -0300

    :package: menu sidebar

---
+++ script.js
@@ -0,0 +1,7 @@
+// var hamburguer = document.querySelector(".hamburguer");
+
+// hamburguer.addEventListener("click", function(){
+//     var siderbar = document.querySelector(".container").classList.toggle("show-menu");
+// })
+
+document.querySelector(".hamburguer").addEventListener("click", () => document.querySelector(".container").classList.toggle("show-menu"))
